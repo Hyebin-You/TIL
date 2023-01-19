dbd69dfb69fa22dfe47a9effe02af10bb0eea70e
binsid1 <binsid1@naver.com>
2023-01-19 17:39:22 +0900

    0119

--- react 온라인 강의 학습 내용/01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ react 온라인 강의 학습 내용/01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card'
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+    // useState는 컴포넌트 함수 안에서 직접적으로 실행되어야 함
+    // 컴포넌트 함수 내의 다른 함수 안에 작성되어서도 안됨
+    const [title, setTitle] = useState(props.title);
+    
     const clickHandler = () => {
-        console.log('Clicked!!!')
+        setTitle('Updated!');
+        console.log(title);
     }
 
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
-                <h2>{ props.title }</h2>
+                <h2>{ title }</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
             <button onClick={clickHandler}>Change Title</button>
