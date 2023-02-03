b558f4029db8382e696a640f8e7a714e6937705c
binsid1 <binsid1@naver.com>
2023-02-03 17:58:25 +0900

    0203 작업물, 공통 깃과 내용물 일치함

---
+++ projects/공통프로젝트/0203 작업물/src/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { AuthContextProvider } from "./store/auth-context";
+import store from './redux/store'
+import { Provider } from 'react-redux'
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import bgMovie from "./assets/video_bg.mp4";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  /*
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+  */
+ <div className="bg_container">
+  <AuthContextProvider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </AuthContextProvider>
+
+  <div className="movie_bg">
+    <video autoPlay loop muted id="bgVideo">
+      <source src={bgMovie} type="video/mp4"/>
+    </video>
+  </div>
+  </div>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
