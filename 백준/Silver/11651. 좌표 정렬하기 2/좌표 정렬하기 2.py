762b019c6fe58fd9054f2033cd239ebe1b7d8fc8
Hyebin-You <109326294+Hyebin-You@users.noreply.github.com>
2023-04-16 14:58:01 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 348 ms, Memory: 59204 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+points = [list(map(int, input().split())) for _ in range(N)]
+
+points.sort(key=lambda x: (x[1], x[0]))
+
+for p in points:
+    print(*p)
