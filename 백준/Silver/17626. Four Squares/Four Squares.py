daec40c76d5e641902014536a788da891c4d0998
Hyebin-You <109326294+Hyebin-You@users.noreply.github.com>
2023-05-30 09:44:19 +0900

    [Silver III] Title: Four Squares, Time: 144 ms, Memory: 115368 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.py
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -2,13 +2,15 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-dp = [10**9] * (n + 1)
+dp = [0] * (n + 1)
 
 dp[0] = 0
 dp[1] = 1
 
 for i in range(2, n + 1):
-    for j in range(1, int(n ** 0.5) + 1):
-        dp[i] = min(dp[i], dp[i-j**2] + 1)
+    min_V = 10**9
+    for j in range(1, int(i ** 0.5) + 1):
+        min_V = min(min_V, dp[i-j**2])
+    dp[i] = min_V + 1
 
 print(dp[n])
